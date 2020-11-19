c2fd21860cc77cf94d1c7d49a7d0caae4a5f76e6
shiguang-dev <shiguang20170617@163.com>
2020-11-19 20:48:15 +0800

    修改Helloword_xinIssue
    就差数据库

--- vuetest/my-vue/src/components/Helloworld_xinIssue.vue
+++ vuetest/my-vue/src/components/Helloworld_xinIssue.vue
@@ -7,17 +7,17 @@
  <el-row :gutter="20">
   <el-col :span="6"><div class="grid-content bg-purple">
   <div>
-  <el-form-item label="Issue No" prop="name">
+  <el-form-item label="Issue No" prop="no" >
   </el-form-item>
   </div>
-  <el-input v-model="ruleForm.no" disabled=""></el-input>
+  <el-input v-model="ruleForm.no" disabled="" placeholder="系统自动生成"></el-input>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
  <div>
- <el-form-item label="创建时间" prop="date1" required>
+ <el-form-item label="创建时间" prop="date1"  >
 </el-form-item>
  </div>
- <el-date-picker v-model="ruleForm.date1" type="date" placeholder="选择日期"  style="width: 100%;" disabled=""></el-date-picker>
+ <el-date-picker v-model="ruleForm.date1" type="date" placeholder="系统自动生成"  style="width: 100%;" disabled=""></el-date-picker>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
   <div>
@@ -46,45 +46,51 @@
   <el-form-item label="影响版本" prop="version">
   </el-form-item>
   </div>
-  <el-input v-model="ruleForm.version" placeholder="内容"></el-input>
+  <el-input v-model="ruleForm.version" placeholder=""></el-input>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
-  <div>
- <el-form-item label="计划修改时间" prop="date2" required>
+  <el-form-item label="计划修改时间" label-width="formLabelWidth" prop="date3"  >
 </el-form-item>
- </div>
  <el-date-picker v-model="ruleForm.date2" type="date" placeholder="内容"  style="width: 100%;" ></el-date-picker>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
    <div>
- <el-form-item label="实际完成时间" prop="date3" required>
+ <el-form-item label="实际完成时间" label-width="formLabelWidth" prop="date3"  >
 </el-form-item>
  </div>
- <el-date-picker v-model="ruleForm.date3" type="date" placeholder="内容"  style="width: 100%;" disabled=""></el-date-picker>
-  </div></el-col>
-  
+ <el-date-picker v-model="ruleForm.date3" type="date" placeholder="系统自动生成"  style="width: 100%;" disabled=""></el-date-picker>
+  </div></el-col> 
 </el-row>
-<el-col>
+<el-row :gutter="20">
+<el-col :span="12"><div class="grid-content bg-purple">
 <div>
 <el-form-item label="重现步骤" prop="step">
     
   </el-form-item>
 </div>
-<el-input v-model="ruleForm.step" placeholder="内容"></el-input>
-</el-col>
-<el-col>
+<el-input type="textarea"
+  :autosize="{ minRows: 2, maxRows: 4}" v-model="ruleForm.step" placeholder="内容"></el-input>
+</div></el-col>
+</el-row>
+
+<el-row :gutter="20">
+<el-col :span="6"><div class="grid-content bg-purple">
 <div>
 <el-form-item label="指派修改人" prop="people">
     
   </el-form-item>
 </div>
 <el-input v-model="ruleForm.people" placeholder="内容"></el-input>
-</el-col>
+</div></el-col>
+</el-row>
 
 <el-form-item style="text-align:center">
-    <el-button  type="primary"   >提交</el-button>
-   
+    <el-button  type="primary"  @click="submitForm('ruleForm')" round>提交</el-button>
+    
   </el-form-item>
+
+
+  
 
 
 
@@ -112,16 +118,16 @@ export default {
             { min: 0, max: 80, message: '长度在 0 到 80 个字符', trigger: 'blur' }
           ],
         no:[
-           { required: false, message: 'Issue No', trigger: 'unchange' }
+           { required: false, message: 'Issue No', trigger: 'change' }
         ],
         data1:[
-          { type: 'date', required: true, message: '创建时间', trigger: 'change' }
+          { type: 'date', required: false, message: '创建时间', trigger: 'change' }
       ],
        data2:[
           { type: 'date', required: true, message: '内容', trigger: 'change' }
       ],
        data3:[
-          { type: 'date', required: true, message: '内容', trigger: 'change' }
+          { type: 'date', required: false, message: '内容', trigger: 'change' }
       ],
       genre:[
             { required: true, message: '内容', trigger: 'blur' },
@@ -143,11 +149,27 @@ export default {
             { min: 0, max: 30, message: '长度在 0 到 30 个字符', trigger: 'blur' }
       ]
     
+    
     }
+  };
+},
+ methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+     
     }
-  },
-}
-
+  }
+     
+    
+  
 
 
 
@@ -171,7 +193,7 @@ export default {
   }
   .grid-content {
     border-radius: 4px;
-    min-height: 10px;
+    min-height: 5px;
   }
   .row-bg {
     padding: 0 0 0 0;
@@ -179,6 +201,9 @@ export default {
   }
 
  
+      
+     
+
     
     
     
