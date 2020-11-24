95f8fdb17562cd8d36bf667893fcc7c10587d2da
shiguang-dev <shiguang20170617@163.com>
2020-11-24 09:34:21 +0800

    hello-xinissue
    111111111

--- vuetest/my-vue/src/components/Helloworld_xinIssue.vue
+++ vuetest/my-vue/src/components/Helloworld_xinIssue.vue
@@ -1,37 +1,37 @@
 <template>
   <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-  <el-form-item label="Issue题目" prop="name">
-    <el-input v-model="ruleForm.name" placeholder="内容"></el-input>
+  <el-form-item label="Issue题目" prop="title">
+    <el-input v-model="ruleForm.title" placeholder="内容"></el-input>
   </el-form-item>
   <div>基本信息<el-divider></el-divider></div>
  <el-row :gutter="20">
   <el-col :span="6"><div class="grid-content bg-purple">
   <div>
-  <el-form-item label="Issue No" prop="no" >
+  <el-form-item label="Issue No" prop="issueId" >
   </el-form-item>
   </div>
-  <el-input v-model="ruleForm.no" disabled="" placeholder="系统自动生成"></el-input>
+  <el-input v-model="ruleForm.issueId" disabled="" placeholder="系统自动生成"></el-input>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
  <div>
- <el-form-item label="创建时间" prop="date1"  >
+ <el-form-item label="创建时间" prop="iCreateTime"  >
 </el-form-item>
  </div>
- <el-date-picker v-model="ruleForm.date1" type="date" placeholder="系统自动生成"  style="width: 100%;" disabled=""></el-date-picker>
+ <el-date-picker v-model="ruleForm.iCreateTime" type="date" placeholder="系统自动生成"  style="width: 100%;" disabled=""></el-date-picker>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
   <div>
-  <el-form-item label="Issue类型" prop="genre">
+  <el-form-item label="Issue类型" prop="type">
   </el-form-item>
   </div>
-  <el-input v-model="ruleForm.genre" placeholder="内容"></el-input>
+  <el-input v-model="ruleForm.type" placeholder="内容"></el-input>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
   <div>
-  <el-form-item label="Issue等级" prop="region">
+  <el-form-item label="Issue等级" prop="grade">
   </el-form-item>
   </div>
-  <el-select v-model="ruleForm.region" placeholder="内容">
+  <el-select v-model="ruleForm.grade" placeholder="内容" style="width: 100%;">
       <el-option label="最高" value="a"></el-option>
       <el-option label="较高" value="b"></el-option>
       <el-option label="一般" value="c"></el-option>
@@ -48,25 +48,24 @@
   </div>
   <el-input v-model="ruleForm.version" placeholder=""></el-input>
   </div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple">
-  <el-form-item label="计划修改时间" label-width="formLabelWidth" prop="date3"  >
+  <el-col :span="6"><div class="grid-content bg-purple" >
+  <el-form-item label="计划修改时间" label-width="formLabelWidth" prop="planTime" required="" >
 </el-form-item>
- <el-date-picker v-model="ruleForm.date2" type="date" placeholder="内容"  style="width: 100%;" ></el-date-picker>
+ <el-date-picker v-model="ruleForm.planTime" type="date" placeholder="内容"  style="width: 100%;" ></el-date-picker>
   </div></el-col>
   <el-col :span="6"><div class="grid-content bg-purple">
    <div>
- <el-form-item label="实际完成时间" label-width="formLabelWidth" prop="date3"  >
+ <el-form-item label="实际完成时间" label-width="formLabelWidth" prop="actualTime"  >
 </el-form-item>
  </div>
- <el-date-picker v-model="ruleForm.date3" type="date" placeholder="系统自动生成"  style="width: 100%;" disabled=""></el-date-picker>
+ <el-date-picker v-model="ruleForm.actualTime" type="date" placeholder="系统自动生成"  style="width: 100%;" disabled=""></el-date-picker>
   </div></el-col> 
 </el-row>
 <el-row :gutter="20">
 <el-col :span="12"><div class="grid-content bg-purple">
 <div>
 <el-form-item label="重现步骤" prop="step">
-    
-  </el-form-item>
+</el-form-item>
 </div>
 <el-input type="textarea"
   :autosize="{ minRows: 2, maxRows: 4}" v-model="ruleForm.step" placeholder="内容"></el-input>
@@ -76,77 +75,109 @@
 <el-row :gutter="20">
 <el-col :span="6"><div class="grid-content bg-purple">
 <div>
-<el-form-item label="指派修改人" prop="people">
-    
-  </el-form-item>
+<el-form-item label="指派修改人" prop="userId2">
+</el-form-item>
 </div>
-<el-input v-model="ruleForm.people" placeholder="内容"></el-input>
+<el-input v-model="ruleForm.userId2" placeholder="内容"></el-input>
 </div></el-col>
 </el-row>
 
+<!--
+<el-row :gutter="20">
+<el-col :span="12"><div class="grid-content bg-purple">
+<div>
+<el-form-item label="解决方案" prop="solution">
+</el-form-item>
+</div>
+<el-input type="textarea"
+  :autosize="{ minRows: 2, maxRows: 4}" v-model="ruleForm.solution" placeholder="内容"></el-input>
+</div></el-col>
+</el-row>
+-->
+
 <el-form-item style="text-align:center">
     <el-button  type="primary"  @click="submitForm('ruleForm')" round>提交</el-button>
-    
-  </el-form-item>
+</el-form-item>
+
+<!--
+  
+
+<el-form-item style="text-align:center">
+    <el-button  type="primary"  @click="submitForm('ruleForm')" round>提交验证</el-button>
+</el-form-item>
+
+
+<el-form-item style="text-align:center">
+    <el-button  type="primary"  @click="submitForm('ruleForm')" round>退回修改</el-button>
+    <el-button :plain="true"  type="primary"  @click="open1" round>关闭</el-button>
+
+</el-form-item>
+-->
 
 
   
 
 
 
-  </el-form>
+</el-form>
 </template>
 <script>
+ import axios from"axios"
 export default {
   data() {
     return {
       ruleForm:{
-        name:'',
-        no:'',
-        data1:'',
-        data2:'',
-         data3:'', 
-        genre:'',
-        region:'',
+        title:'',
+        issueId:'',
+        iCreateTime:'',
+        planTime:'',
+        actualTime:'', 
+        type:'',
+        grade:'',
         version:'',
         step:'',
-        people:''
+        userId2:'',
+        solution:''
       },
       rules:{
-        name: [
-            { required: true, message: '内容', trigger: 'blur' },
+        title: [
+            { required: true, message: '请输入Issue标题', trigger: 'blur' },
             { min: 0, max: 80, message: '长度在 0 到 80 个字符', trigger: 'blur' }
           ],
-        no:[
-           { required: false, message: 'Issue No', trigger: 'change' }
-        ],
-        data1:[
-          { type: 'date', required: false, message: '创建时间', trigger: 'change' }
+      //  issueId:[
+      //      { required: false, message: '请输入Issue标题', trigger: 'change' }
+      //   ],
+      //   iCreateTime:[
+      //     { type: 'date', required: false, message: '创建时间', trigger: 'change' }
+      // ],
+       planTime:[
+          { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
       ],
-       data2:[
-          { type: 'date', required: true, message: '内容', trigger: 'change' }
-      ],
-       data3:[
-          { type: 'date', required: false, message: '内容', trigger: 'change' }
-      ],
-      genre:[
-            { required: true, message: '内容', trigger: 'blur' },
+      //  actualTime:[
+      //     { type: 'date', required: false, message: '内容', trigger: 'change' }
+      // ],
+      type:[
+            { required: true, message: '请输入Issue类型', trigger: 'blur' },
             { min: 0, max: 30, message: '长度在 0 到 30 个字符', trigger: 'blur' }
       ],
-      region:[
-        { required: true, message: '内容', trigger: 'change' }
+      grade:[
+        { required: true, message: '请选择一个Issue等级', trigger: 'change' }
       ],
       version:[
-            { required: true, message: '内容', trigger: 'blur' },
+            { required: true, message: '请输入影响版本', trigger: 'blur' },
             { min: 0, max: 30, message: '长度在 0 到 30 个字符', trigger: 'blur' }
       ],
       step:[
-        { required: true, message: '内容', trigger: 'blur' },
+        { required: true, message: '请输入步骤', trigger: 'blur' },
             { min: 0, max: 2000, message: '长度在 0 到 2000 个字符', trigger: 'blur' }
       ],
-      people:[
-        { required: true, message: '内容', trigger: 'blur' },
+      userId2:[
+        { required: true, message: '请指定修改人', trigger: 'blur' },
             { min: 0, max: 30, message: '长度在 0 到 30 个字符', trigger: 'blur' }
+      ],
+      solution:[
+        { required: true, message: '请输入解决方案', trigger: 'blur' },
+            { min: 0, max: 2000, message: '长度在 0 到 2000 个字符', trigger: 'blur' }
       ]
     
     
@@ -158,6 +189,46 @@ export default {
         this.$refs[formName].validate((valid) => {
           if (valid) {
             alert('submit!');
+            
+
+
+
+
+
+            let data = {
+    // 'issueId':this.ruleForm.issueId,
+    'title':this.ruleForm.title,
+    // 'iCreateTime':this.ruleForm.iCreateTime,
+    'userId2':this.ruleForm.userId2,
+    'planTime':this.ruleForm.planTime,
+    // 'actualTime':this.ruleForm.actualTime,
+    'type':this.ruleForm.type,
+    'grade':this.ruleForm.grade,
+    'version':this.ruleForm.version,
+    'step':this.ruleForm.step,
+    
+  };
+axios.post('api/issue/insertIssue',data)
+
+     .then(res=>{
+         console.log('res=>',res);  
+         alert('hhh')   
+         console.log(data)      ; 
+         }).catch(err => {
+        console.log(err);
+        alert('xxx')  
+       });
+      
+
+
+
+
+
+
+
+
+
+
           } else {
             console.log('error submit!!');
             return false;
@@ -165,10 +236,22 @@ export default {
         });
       },
      
-    }
-  }
-     
+
+
+  
+  
+
     
+   }
+}
+ 
+ 
+   
+   
+ 
+
+
+ 
   
 
 
